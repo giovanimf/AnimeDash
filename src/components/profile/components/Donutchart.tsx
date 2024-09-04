5895f7c74bea105df1c5ac30f1f04df84253f2e2
giovanimf <giovani.mf@hotmail.com>
2024-09-04 18:42:03 -0300

    atualização widget de acessos

--- src/components/profile/components/Donutchart.tsx
+++ src/components/profile/components/Donutchart.tsx
@@ -1,5 +1,6 @@
 import Chart from 'react-apexcharts';
 import { ApexOptions } from 'apexcharts';
+import Title from '../../../ui/title';
 
 interface DarkModeProps {
   darkMode: boolean;
@@ -43,7 +44,8 @@ export function DonutChart({ darkMode }: DarkModeProps) {
   const series: number[] = [44, 55, 41];
 
   return (
-    <div className="py-6 bg-white rounded-lg p-5 flex dark:bg-gray-600 items-center justify-center">
+    <div className="py-6 bg-white rounded-lg p-5 flex dark:bg-gray-600 items-start justify-center flex-col">
+      <Title>Acessos</Title>
       <Chart options={options} series={series} type="donut" height={350} />
     </div>
   );
